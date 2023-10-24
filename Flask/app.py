f585004752bd202ddc798bca0841c6859e28d5a9
JoaoMagalhaes-hub <jgabrielomm@gmail.com>
2023-10-23 21:28:28 -0300

    Eu odeio Flask

--- Flask/app.py
+++ Flask/app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.rout('/')
+@app.route('/')
 def ola():
-    return '<h1> Olá, mundo! </h1>'
+    return render_template('index.html')
+
 
 
 app.run()
